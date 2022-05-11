f30f3e048fa5d07a64b9ca9aecfb3198f425575d
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-05-11 16:34:48 +0545

    implement signature verification on vote creation

--- src/controllers/vote.js
+++ src/controllers/vote.js
@@ -1,9 +1,46 @@
 const Vote = require("../models/Vote");
+const User = require("../models/User");
+
+const generateNonce = require("../utils/generateNonce");
+const { Account } = require("@commandokoala/thenewboston");
 
 const createVote = async (req, res) => {
   try {
-    const vote = await Vote.create(req.body);
-    return res.json(vote);
+    const { accountNumber, signature } = req.body;
+
+    const user = await User.findOne({ accountNumber });
+
+    if (!user) {
+      return res.json({
+        error:
+          "User validation failed: User associated with `accountNumber` does not exist..",
+      });
+    }
+
+    const message = {
+      choices: req.body.choices,
+      poll: req.body.poll,
+      nonce: user.nonce,
+    };
+
+    const stringifiedMessage = JSON.stringify(message);
+
+    const isValidSignature = Account.verifySignature(
+      stringifiedMessage,
+      signature,
+      accountNumber
+    );
+
+    if (isValidSignature) {
+      const vote = await Vote.create(req.body);
+      user.nonce = generateNonce();
+      await user.save();
+      return res.json(vote);
+    } else {
+      return res.json({
+        error: "Invalid Signature..",
+      });
+    }
   } catch (err) {
     res.json(err);
   }
