6c29e558b9d690b095a398c6582ac35779e6ddbb
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-05-21 22:17:25 +0545

    feat: script to calculate poll results

--- scripts/calculate_poll_results.py
+++ scripts/calculate_poll_results.py
@@ -1,4 +1,8 @@
 import requests
+import json
+import os
+from nacl.encoding import HexEncoder
+import nacl.signing
 
 BASE_API_URL = "https://7nfr0m.deta.dev"
 LEAPCHAIN_BALANCE_API_URL = "https://raw.githubusercontent.com/LeapChain/Account-Backups/master/account_backups/2022-04-17-15%3A08%3A28.json"
@@ -43,6 +47,12 @@ def get_account_balance(account_number):
         return account_balances[account_number]['balance']
     return 0
 
+def get_user_nonce(account_number):
+    body = {
+        "accountNumber": account_number
+    }
+    return requests.post(url=f"{BASE_API_URL}/api/v1/users", json=body, headers={}).json()['nonce']
+
 
 def list_active_polls():
     return fetch(url=f"{BASE_API_URL}/api/v1/polls?status=0", headers={})
@@ -59,21 +69,61 @@ def calculate_vote_results():
     for poll in active_polls:
 
         poll_vote_weightage = 0
-        choice_vote_weightage = {}
         all_votes_on_poll = list_votes_on_poll(poll['_id'])['votes']
+        temp_choices = []
+        temp_choice_dict = {}
+        choices = []
 
         for vote in all_votes_on_poll:
 
             voter_account_balance = get_account_balance(vote['accountNumber'])
             poll_vote_weightage += voter_account_balance
 
-            if vote['choices'] in choice_vote_weightage:
-                choice_vote_weightage[vote['choices']] += voter_account_balance
-            else:
-                choice_vote_weightage[vote['choices']] = voter_account_balance
+            temp_choices.append({
+                "_id": vote['choices'],
+                "totalVotes": voter_account_balance
+                })
 
-        print(choice_vote_weightage)
-        print(poll_vote_weightage)
+        # merge duplicate choices and add the votes..
+        for individual_choice in temp_choices:
+            if individual_choice["_id"] in temp_choice_dict:
+                temp_choice_dict[individual_choice["_id"]] += individual_choice["totalVotes"]
+            else:
+                temp_choice_dict[individual_choice["_id"]] = individual_choice["totalVotes"]
+
+        # convert the temp_choice_dict to list of choice with values
+        for choice in temp_choice_dict:
+            choices.append({
+                "_id": choice,
+                "totalVotes": temp_choice_dict[choice]
+            })
+        
+        # sort the choices by ascending "_id" parameter
+        sorted_choices = sorted(choices, key=lambda x: x['_id'])
+
+        # sign the message using private key
+        signing_key = nacl.signing.SigningKey(str.encode(os.environ['PRIVATE_KEY']), encoder=nacl.encoding.HexEncoder)
+        account_number = signing_key.verify_key.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8')
+
+        message = {
+            "voteWeightage": poll_vote_weightage,
+            "nonce": get_user_nonce(account_number),
+            "choices": sorted_choices,
+        }
+
+        json_message = json.dumps(message, separators=(',', ':'))
+
+        signed_hex = signing_key.sign(str.encode(json_message), encoder=HexEncoder)
+        signature = signed_hex.signature.decode('utf-8')
+
+        patch_request_body = {}
+        patch_request_body['accountNumber'] = account_number
+        patch_request_body['signature'] = signature
+        patch_request_body['voteWeightage'] = poll_vote_weightage
+        patch_request_body['choices'] = sorted_choices
+        update_poll = requests.patch(url=f"{BASE_API_URL}/api/v1/polls/{poll['_id']}", json=patch_request_body, headers={}).json()
+        print(update_poll)
+        print("------------------------------------")
 
 
 calculate_vote_results()
