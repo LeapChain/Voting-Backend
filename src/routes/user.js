478f7108359c09a0cc67b1c77e952068f7317d08
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-05-23 08:38:38 +0545

    move create users to /api/v1/users/create endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controllers/user");
 
-router.post("/", createUser);
+router.post("/create", createUser);
 
 module.exports = router;
