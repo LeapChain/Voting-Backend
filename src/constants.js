430128fa0560ff6f72a3b1bdaeb9ad5f19ddd25b
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-06-10 12:55:40 +0545

    added hussu's account in the whitelist

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 WHITELISTEAD_POLL_ACCOUNT_NUMBERS = [
-  "3b2cb4e9276a06bafafd2344861df480fb68abf8cebac1025646cce3907fd422",
+  "3b2cb4e9276a06bafafd2344861df480fb68abf8cebac1025646cce3907fd422", // github actions account number
+  "22d0f0047b572a6acb6615f7aae646b0b96ddc58bfd54ed2775f885baeba3d6a", // hussu's wallet
 ];
 
 module.exports = {
