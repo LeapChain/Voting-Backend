9b98e8e4b28125db380dabd2d81b8e88231d790d
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-06-11 15:30:16 +0545

    added mrsky's wallet as whitelisted account number

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 WHITELISTEAD_POLL_ACCOUNT_NUMBERS = [
   "3b2cb4e9276a06bafafd2344861df480fb68abf8cebac1025646cce3907fd422", // github actions account number
   "22d0f0047b572a6acb6615f7aae646b0b96ddc58bfd54ed2775f885baeba3d6a", // hussu's wallet
+  "dfb24138584042044305f9417e4fe02ca4bde604da6cb2e211a825a2ab5a4e3c", // mrsky's wallet
 ];
 
 module.exports = {
