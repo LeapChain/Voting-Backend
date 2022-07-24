2e19cf560fb5f8dfca58bfc436e32959178d3c6d
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-07-24 21:13:33 +0545

    depreciate POST /votes endpoint
    use POST /polls/:id/vote endpoint for casting the poll vote.
    use middleware to check if accountNumber exists and to verify the
    signature of the message.

--- src/routes/poll.js
+++ src/routes/poll.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const isAdminAccount = require("../middleware/isAdminAccount");
 const { PollSchema } = require("../schema/pollSchema");
+const { pollVoteSchema } = require("../schema/voteSchema");
 const {
   validateRequestSchema,
 } = require("../middleware/validateRequestSchema");
 const { validateSignature } = require("../middleware/validateSignature");
+const userExists = require("../middleware/userExists");
 
 const {
   getAllPoll,
@@ -13,8 +15,10 @@ const {
   createPoll,
   updatePoll,
 } = require("../controllers/poll");
+const { createPollVote } = require("../controllers/vote");
 
 router.get("/", getAllPoll);
+
 router.post(
   "/",
   PollSchema,
@@ -23,7 +27,18 @@ router.post(
   validateSignature,
   createPoll
 );
+
 router.get("/:id", getPoll);
+
 router.patch("/:id", isAdminAccount, updatePoll);
+
+router.post(
+  "/:id/vote",
+  pollVoteSchema,
+  validateRequestSchema,
+  validateSignature,
+  userExists,
+  createPollVote
+);
 
 module.exports = router;
