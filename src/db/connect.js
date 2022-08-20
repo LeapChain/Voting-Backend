8c23165d4aa24838118b7e84f0a66ae2552e7ba0
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-08-20 11:47:18 +0545

    define connection settings for jest

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to db successfully");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connect(process.env.MONGO_URI).catch((error) => {
+  console.log(error);
+});
