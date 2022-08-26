99cdc1e4b9865a7a508faa145ae67dc36673a3b2
hussu010 <55182298+dhakalkushal@users.noreply.github.com>
2022-08-26 13:12:04 +0545

    use updateOne to update user type since we're not returning

--- src/utils/fetchAndProcessTransactions.js
+++ src/utils/fetchAndProcessTransactions.js
@@ -91,7 +91,7 @@ const processTransactions = async () => {
 
       if (user) {
         if (confirmedTransaction.amount >= GOVERNOR_REQUEST_FEE) {
-          User.findOneAndUpdate({ _id: userId }, { type: UserType.GOVERNER });
+          await User.updateOne({ _id: userId }, { type: UserType.GOVERNER });
 
           const governorRequest = await GovernorRequest.findOne({
             accountNumber: user.accountNumber,
