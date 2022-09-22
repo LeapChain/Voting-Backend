f27b3a7de06a7d36bfea0aabde2e9c75d57df84c
hussu <hussu@fetch.ly>
2022-09-22 14:39:05 +0545

    Revert "generate swagger docs on every server restart"
    This reverts commit 928322b8114ac991f87c24c9afae9fbcb4b4979b.

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ if (process.env.NODE_ENV !== "test") {
   require("./src/db/connect");
 }
 
-require("./swagger/swagger");
-
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
